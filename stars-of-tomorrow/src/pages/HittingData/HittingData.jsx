ad4dfcf4e546e934457446792c14972a8b8f79a9
Gunnar Smith <gunnarsmith3@gmail.com>
2023-03-28 14:27:47 -0500

    hitting form style

--- stars-of-tomorrow/src/pages/HittingData/HittingData.jsx
+++ stars-of-tomorrow/src/pages/HittingData/HittingData.jsx
@@ -1,4 +1,4 @@
-import { Typography, Box, Stack, Button, Grid, Container, TextField } from '@mui/material';
+import { Typography, Box, Stack, Button, Grid, Container, TextField, Paper } from '@mui/material';
 import Navbar from '../../components/NavBar/Navbar'
 
 import { useState } from 'react';
@@ -142,7 +142,6 @@ const HittingData = () => {
 
     return (
         <Container>
-            <Navbar />
             <Box display='flex' aligndivs='center' justifyContent='center'>
                 <Typography
                     justifyContent='center'
@@ -161,55 +160,66 @@ const HittingData = () => {
                 </Typography>
             </Box>
 
-
-            <Grid container spacing={2}>
-                <Grid item xs={4}>
+            <Box display='flex' justifyContent='center'>
+            <Paper sx={{ width: '500px', px: '10px', py: '10px' }} style={{ background: "-webkit-linear-gradient(45deg, #000 30%, #60696b 90%)",}}>
+            <Grid container spacing={2} display='flex' sx={{marginTop: '.2rem'}}>
+                <Grid item xs={6}>
                     <Typography
+                        display='flex'
+                        justifyContent='flex-end'
                         variant='h5'
                         sx={{
                             color: 'white',
                             fontFamily: 'Oswald, sans-serif',
                             letterSpacing: '.5px',
-                            fontWeight: 300
+                            fontWeight: 300,
+                            marginRight: '1.5rem'
                         }}
                     >
-                        {exAthlete.name}
+                        Player: {exAthlete.name}
                     </Typography>
                 </Grid>
-                <Grid item xs={8}>
+                <Grid item xs={6}>
                     <Typography
                         variant='h5'
                         sx={{
                             color: 'white',
                             fontFamily: 'Oswald, sans-serif',
                             letterSpacing: '.5px',
-                            fontWeight: 300
+                            fontWeight: 300,
+                            marginLeft: '2rem'
                         }}
                     >
-                        Number: {exAthlete.number}
+                        Camp Number: {exAthlete.number}
                     </Typography>
                 </Grid>
-                <Grid item xs={4}>
+                <Grid item xs={6}>
                     <Typography
+                        display='flex'
+                        justifyContent='center'
                         variant='h5'
                         sx={{
                             color: 'white',
                             fontFamily: 'Oswald, sans-serif',
                             letterSpacing: '.5px',
-                            fontWeight: 300
+                            fontWeight: 300,
+                            marginRight: '2rem'
                         }}
                     >
                         Position: {exAthlete.position}
                     </Typography>
                 </Grid>
-                <Grid item xs={8}>
+                <Grid item xs={6}>
                     <Typography
+                        display='flex'
+                        justifyContent='center'
                         variant='h5'
                         sx={{
                             color: 'white',
                             fontFamily: 'Oswald, sans-serif',
                             letterSpacing: '.5px',
-                            fontWeight: 300
+                            fontWeight: 300,
+                            marginRight: '4.5rem'
                         }}
                     >
                         Class: {exAthlete.class}
@@ -217,61 +227,57 @@ const HittingData = () => {
                 </Grid>
                 <Grid item xs={4}>
                     <Typography
+                        display='flex'
+                        justifyContent='center'
                         variant='h5'
                         sx={{
                             color: 'white',
                             fontFamily: 'Oswald, sans-serif',
                             letterSpacing: '.5px',
-                            fontWeight: 300
+                            fontWeight: 300,
+                            marginLeft: '2.7rem',
+                            marginBottom: '1rem'
                         }}
                     >
                         Bats: {exAthlete.bats}
                     </Typography>
                 </Grid>
             </Grid>
-
-            <Box display='flex' aligndivs='center' justifyContent='center'>
-                <Grid container spacing={2} justifyContent={'center'} marginBottom={4} >
-                    <Grid item xs={9}>
-                        <Typography
-                            justifyContent='center'
-                            variant='h4'
-                            style={{
-                                background: "-webkit-linear-gradient(45deg, #f2cd00 30%, #ffffff 90%)",
-                                WebkitBackgroundClip: "text",
-                                WebkitTextFillColor: "transparent",
-                            }}
-                            sx={{ letterSpacing: '.5px', fontFamily: 'Oswald, sans-serif', fontWeight: 500, }}
-                            marginTop={5}
-                            textAlign='center'
-                        >
-                            Scouting Evaluation
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={2}>
-                        <Typography
-                            justifyContent='center'
-                            variant='h4'
-                            style={{
-                                background: "-webkit-linear-gradient(45deg, #f2cd00 30%, #ffffff 90%)",
-                                WebkitBackgroundClip: "text",
-                                WebkitTextFillColor: "transparent",
-                            }}
-                            sx={{ letterSpacing: '.5px', fontFamily: 'Oswald, sans-serif', fontWeight: 500, }}
-                            marginTop={5}
-                            textAlign='center'
-                        >
-                            Grade
-                        </Typography>
-                    </Grid>
-                </Grid>
+            </Paper>
             </Box>
 
-
-
-
-            <Grid container spacing={2} justifyContent={'center'}>
-                <Grid item xs={9}>
+            <Box display='flex' alignItems='center' justifyContent='space-between' sx={{ marginTop: '3rem'}}>
+            <Typography
+                justifyContent='center'
+                variant='h4'
+                style={{
+                    background: "-webkit-linear-gradient(45deg, #f2cd00 30%, #ffffff 90%)",
+                    WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+                        }}
+                sx={{ marginBottom: '1rem', marginRight: '4rem', marginLeft: '2rem', letterSpacing: '.5px', fontFamily: 'Oswald, sans-serif', fontSize:'30px', fontWeight: 500, }}
+                    >
+                    Scouting Evaluation
+            </Typography>
+            <Typography
+                justifyContent='center'
+                variant='h4'
+                style={{
+                    background: "-webkit-linear-gradient(45deg, #f2cd00 30%, #ffffff 90%)",
+                    WebkitBackgroundClip: "text",
+                    WebkitTextFillColor: "transparent",
+                    }}
+                sx={{ marginBottom: '1rem', marginRight: '2.7rem', letterSpacing: '.5px', fontFamily: 'Oswald, sans-serif', fontWeight: 500, }}
+                    >
+                    Grade
+            </Typography>
+            </Box>
+            <Grid container>
+            <Grid item xs={12}>
+            <Box display='flex' aligndivs='center' marginBottom='2rem'>
+                <Paper style={{ background: "-webkit-linear-gradient(45deg, #000 30%, #60696b 90%)",}}>
+                <Grid container spacing={2} justifyContent={'center'} paddingX='1rem' paddingY='1rem' >
+                    <Grid item xs={9}>
                     <Typography
                         variant='h5'
 
@@ -284,10 +290,10 @@ const HittingData = () => {
                     >
                         Hitting:
                     </Typography>
-                </Grid>
-                <Grid item xs={3}>
+                    </Grid>
+                    <Grid item xs={3}>
                     <Grid container justifyContent={'center'}>
-                        <Button sx={{ color: 'yellow', fontFamily: 'proxima-noba, Verdana, Arial, sans-serif', fontWeight: 900, background: 'black' }} onClick={subtractHitting}>-</Button>
+                        <Button sx={{ borderRadius: '15px', color: '#f5f6fa', fontFamily: 'proxima-noba, Verdana, Arial, sans-serif', fontWeight: 900, background: 'black' }} onClick={subtractHitting}>-</Button>
                         <Typography
                             justifyContent='center'
                             variant='h4'
@@ -302,7 +308,7 @@ const HittingData = () => {
                         >
                             {hitting}
                         </Typography>
-                        <Button sx={{ color: 'yellow', fontFamily: 'proxima-noba, Verdana, Arial, sans-serif', fontWeight: 900, background: 'black' }} onClick={addHitting}>+</Button>
+                        <Button sx={{ borderRadius: '15px', color: '#f5f6fa', fontFamily: 'proxima-noba, Verdana, Arial, sans-serif', fontWeight: 900, background: 'black' }} onClick={addHitting}>+</Button>
                     </Grid>
 
                 </Grid>
@@ -322,7 +328,7 @@ const HittingData = () => {
                 </Grid>
                 <Grid item xs={3}>
                     <Grid container justifyContent={'center'}>
-                        <Button sx={{ color: 'yellow', fontFamily: 'proxima-noba, Verdana, Arial, sans-serif', fontWeight: 900, background: 'black' }} onClick={subtractSwingOperation}>-</Button>
+                        <Button sx={{ borderRadius: '15px', color: '#f5f6fa', fontFamily: 'proxima-noba, Verdana, Arial, sans-serif', fontWeight: 900, background: 'black' }} onClick={subtractSwingOperation}>-</Button>
                         <Typography
                             justifyContent='center'
                             variant='h4'
@@ -337,7 +343,7 @@ const HittingData = () => {
                         >
                             {swingOperation}
                         </Typography>
-                        <Button sx={{ color: 'yellow', fontFamily: 'proxima-noba, Verdana, Arial, sans-serif', fontWeight: 900, background: 'black' }} onClick={addSwingOperation}>+</Button>
+                        <Button sx={{ borderRadius: '15px', color: '#f5f6fa', fontFamily: 'proxima-noba, Verdana, Arial, sans-serif', fontWeight: 900, background: 'black' }} onClick={addSwingOperation}>+</Button>
                     </Grid>
 
                 </Grid>
@@ -357,7 +363,7 @@ const HittingData = () => {
                 </Grid>
                 <Grid item xs={3}>
                     <Grid container justifyContent={'center'}>
-                        <Button sx={{ color: 'yellow', fontFamily: 'proxima-noba, Verdana, Arial, sans-serif', fontWeight: 900, background: 'black' }} onClick={subtractPower}>-</Button>
+                        <Button sx={{ borderRadius: '15px', color: '#f5f6fa', fontFamily: 'proxima-noba, Verdana, Arial, sans-serif', fontWeight: 900, background: 'black' }} onClick={subtractPower}>-</Button>
                         <Typography
                             justifyContent='center'
                             variant='h4'
@@ -372,7 +378,7 @@ const HittingData = () => {
                         >
                             {power}
                         </Typography>
-                        <Button sx={{ color: 'yellow', fontFamily: 'proxima-noba, Verdana, Arial, sans-serif', fontWeight: 900, background: 'black' }} onClick={addPower}>+</Button>
+                        <Button sx={{ borderRadius: '15px', color: '#f5f6fa', fontFamily: 'proxima-noba, Verdana, Arial, sans-serif', fontWeight: 900, background: 'black' }} onClick={addPower}>+</Button>
                     </Grid>
 
                 </Grid>
@@ -392,7 +398,7 @@ const HittingData = () => {
                 </Grid>
                 <Grid item xs={3}>
                     <Grid container justifyContent={'center'}>
-                        <Button sx={{ color: 'yellow', fontFamily: 'proxima-noba, Verdana, Arial, sans-serif', fontWeight: 900, background: 'black' }} onClick={subtractSwingPath}>-</Button>
+                        <Button sx={{ borderRadius: '15px', color: '#f5f6fa', fontFamily: 'proxima-noba, Verdana, Arial, sans-serif', fontWeight: 900, background: 'black' }} onClick={subtractSwingPath}>-</Button>
                         <Typography
                             justifyContent='center'
                             variant='h4'
@@ -407,7 +413,7 @@ const HittingData = () => {
                         >
                             {swingPath}
                         </Typography>
-                        <Button sx={{ color: 'yellow', fontFamily: 'proxima-noba, Verdana, Arial, sans-serif', fontWeight: 900, background: 'black' }} onClick={addSwingPath}>+</Button>
+                        <Button sx={{ borderRadius: '15px', color: '#f5f6fa', fontFamily: 'proxima-noba, Verdana, Arial, sans-serif', fontWeight: 900, background: 'black' }} onClick={addSwingPath}>+</Button>
                     </Grid>
 
                 </Grid>
@@ -427,7 +433,7 @@ const HittingData = () => {
                 </Grid>
                 <Grid item xs={3}>
                     <Grid container justifyContent={'center'}>
-                        <Button sx={{ color: 'yellow', fontFamily: 'proxima-noba, Verdana, Arial, sans-serif', fontWeight: 900, background: 'black' }} onClick={subtractBarrelContact}>-</Button>
+                        <Button sx={{ borderRadius: '15px',  color: '#f5f6fa', fontFamily: 'proxima-noba, Verdana, Arial, sans-serif', fontWeight: 900, background: 'black', }} onClick={subtractBarrelContact}>-</Button>
                         <Typography
                             justifyContent='center'
                             variant='h4'
@@ -442,11 +448,17 @@ const HittingData = () => {
                         >
                             {barrelContact}
                         </Typography>
-                        <Button sx={{ color: 'yellow', fontFamily: 'proxima-noba, Verdana, Arial, sans-serif', fontWeight: 900, background: 'black' }} onClick={addBarrelContact}>+</Button>
+                        <Button sx={{ borderRadius: '15px', color: '#f5f6fa', fontFamily: 'proxima-noba, Verdana, Arial, sans-serif', fontWeight: 900, background: 'black' }} onClick={addBarrelContact}>+</Button>
                     </Grid>
 
                 </Grid>
-                <Grid item xs={6}>
+                </Grid>
+                </Paper>
+            </Box>
+            </Grid>
+            </Grid>
+
+            <Grid item xs={6}>
                     <Typography
                         variant='h5'
 
@@ -454,37 +466,35 @@ const HittingData = () => {
                             color: 'white',
                             fontFamily: 'Oswald, sans-serif',
                             letterSpacing: '.5px',
-                            fontWeight: 300
+                            fontWeight: 300,
+                            marginLeft: '.5rem'
                         }}
                     >
                         Additional Notes:
                     </Typography>
                 </Grid>
-                <Grid item xs={6}>
+                <Grid item xs={12}>
                     <Box
                         component="form"
                         sx={{
-                            '& .MuiTextField-root': { m: 1, width: '50ch', backgroundColor: 'white', fontWeight: 300 },
+                            '& .MuiTextField-root': { m: 1, width: '100%', backgroundColor: 'white', fontWeight: 300 },
                         }}
                         noValidate
                         autoComplete="off"
                     >
-                        <TextField fullWidth
+                        <TextField 
+                            fullWidth
                             id="standard-multiline-flexible"
                             multiline
-                            minRows={4}
+                            minRows={6}
                             variant="filled"
                             defaultValue={'notes'}
                             value={notes} onChange={(event) => setNotes(event.currentTarget.value)}
-
-
-
+                            sx={{ borderRadius: '4px'}}
                         />
                     </Box>
 
                 </Grid>
-            </Grid>
-
 
         </Container>
 
